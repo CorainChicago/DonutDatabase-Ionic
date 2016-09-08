2b88671d5c3cda1b6e6ef5f575f5793017323583
CorainChicago <chaysmagan@gmail.com>
2016-09-08 15:10:33 -0500

    removed un-used code from controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,10 @@
 angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope) {})
-.controller('DonutsCtrl', function($scope, $location) {
-  $scope.go = function ( path ) {
-    $location.path( path );
-  };
-})
+.controller('DonutsCtrl', function($scope) {})
 
-.controller('DonutShopsCtrl', function($scope, Chats) {
+.controller('DonutsNewCtrl', function($scope) {})
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+.controller('DonutShopsCtrl', function($scope) {})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AboutCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+.controller('AboutCtrl', function($scope) {});
